5ae06237a3e11be0df2b2f372a6449c0a2e61cc0
exemon <exemondev@gmail.com>
2024-03-30 16:21:32 +0100

    Init for the project. Set the CMake and ncurses

---
+++ compiler_flags.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a -pedantic-errors -Wall -Werror -Wshadow -Weffc++ -Wextra -Wsign-conversion -Wunused-result -fsanitize=address,undefined")
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a  -Wshadow -Weffc++ -fsanitize=address,undefined")
