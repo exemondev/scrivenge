a65deffe8e6f23621fea4a3b798b04eacd0d7343
exemon <exemondev@gmail.com>
2024-03-30 16:34:03 +0100

    Add the enum for all of the windows types

---
+++ includes/view_window_type.hpp
@@ -0,0 +1,15 @@
+#ifndef VIEW_WINDOW_TYPE_HPP
+#define VIEW_WINDOW_TYPE_HPP
+
+namespace View
+{
+    enum class WindowType
+    {
+        SwitchFile,
+        OpenFile
+        NumLine,
+        SourceCode
+    };
+}
+
+#endif
